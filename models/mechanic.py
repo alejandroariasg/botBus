3b1c09520bc3f50a78b58323ef72391ee497f276
sgranadaag <santiagogranadaaguirre@gmail.com>
2023-01-31 19:44:57 -0500

    feat: add record class

--- models/mechanic.py
+++ models/mechanic.py
@@ -1,12 +1,12 @@
 import database.db as db
-from sqlalchemy import Column, Float
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
 class Mechanic(db.Base):
     __tablename__ = 'mechanic'
-    id = Column('id', Float, primary_key=True, nullable=False)
-    phone = Column('phone', Float, server_default='0', nullable=False)
+    id = Column('id', String, primary_key=True, nullable=False, unique=True)
+    phone = Column('phone', Integer, server_default='0', nullable=False)
     maintenances = relationship('Maintenance', back_populates='mechanic')
 
     def __init__(self, id, phone):
---
+++ record.py
@@ -0,0 +1,7 @@
+class Record():
+    user_id = None
+    user_type = None
+
+    def set_user_data(self, user_id, user_type):
+        self.user_id = user_id
+        self.user_type = user_type
