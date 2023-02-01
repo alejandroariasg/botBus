046397a97db558e8b764b45d388a351e36a8366b
sgranadaag <santiagogranadaaguirre@gmail.com>
2023-01-31 19:41:13 -0500

    feat: add auth middleware

--- logic.py
+++ logic.py
@@ -5,9 +5,26 @@ from models.insurance import Insurance
 from models.maintenance import Maintenance
 from models.owner import Owner
 from models.vehicle import Vehicle
+from record import Record
+record = Record()
 
 
-#### register
+def login_user(user_id):
+    mechanic = db.session.query(Mechanic).get(user_id)
+    db.session.commit()
+    if mechanic != None:
+        record.set_user_data(user_id, 'mechanic')
+        return 'mechanic'
+    else:
+        owner = db.session.query(Mechanic).get(user_id)
+        db.session.commit()
+        if owner != None:
+            record.set_user_data(user_id, 'owner')
+            return 'owner'
+    return False
+
+
+# register
 def register_mechanic(user_id, phone):
     mechanic = db.session.query(Mechanic).get(user_id)
     db.session.commit()
@@ -18,3 +35,17 @@ def register_mechanic(user_id, phone):
         return True
     return False
 
+
+def auth_middleware(func):
+    def wrapper(user_id, *args):
+        if (record.user_id):
+            func(*args, user_id)
+        else:
+            login_user(user_id)
+            func(*args, user_id)
+    return wrapper
+
+
+@auth_middleware
+def func_prueb(user_id):
+    print("dflkfdklfd" + str(record.user_id))
